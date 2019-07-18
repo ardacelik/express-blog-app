cfa31c62d502e858af35d07389f4df66ed225f44
Arda Celik <arda.celik@uoit.net>
2019-07-18 12:45:04 -0400

    Blog template

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const app = express(); // Create an express app\
+const app = express(); // Create an express app
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "pug"); // Set the view engine to Pug
 
@@ -13,6 +17,10 @@ app.get("/blog", (req, res) => {
     prompt: "dear user",
     hint: "Do not overthink it, just start writing :)"
   });
+});
+
+app.post("/blog", (req, res) => {
+  res.render("blog");
 });
 
 app.listen(3000, () => {

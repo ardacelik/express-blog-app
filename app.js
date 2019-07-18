fe8b1ed9290a74f99c994d1a6c184b82dcbf6256
Arda Celik <arda.celik@uoit.net>
2019-07-17 23:16:55 -0400

    Render the index and blog templates

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const app = express(); // Create an express app\
 app.set("view engine", "pug"); // Set the view engine to Pug
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.render("index");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<h1>Hello Developer</h1>");
+app.get("/blog", (req, res) => {
+  res.render("blog", {
+    prompt: "dear user",
+    hint: "Do not overthink it, just start writing :)"
+  });
 });
 
 app.listen(3000, () => {

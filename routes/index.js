39a68ba263a19ea78fa1c940b9516a4d76293466
Arda Celik <arda.celik@uoit.net>
2019-07-21 21:32:22 -0400

    Register a user in the database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
 
 // GET /register
 router.get("/register", (req, res, next) => {
@@ -8,7 +9,39 @@ router.get("/register", (req, res, next) => {
 
 // POST /register
 router.post("/register", (req, res, next) => {
-  return res.send("User created!");
+  if (
+    req.body.email &&
+    req.body.name &&
+    req.body.password &&
+    req.body.confirmPassword
+  ) {
+    // Confirm that user types dome password twice
+    if (req.body.password !== req.body.confirmPassword) {
+      const err = new Error("Passwords do not match.");
+      err.status = 400;
+      return next(err);
+    }
+
+    // Create object with form input
+    const userData = {
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password
+    };
+
+    // Use schema's 'create' method to insert document into Mongo
+    User.create(userData, function(error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        return res.redirect("/profile");
+      }
+    });
+  } else {
+    const err = new Error("All fields required.");
+    err.status = 400;
+    return next(err);
+  }
 });
 
 // GET /

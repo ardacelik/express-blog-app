5b81ffb2479b70d43bccfb54f6f196b93f855b02
Arda Celik <arda.celik@uoit.net>
2019-07-24 10:46:30 -0400

    Changed some of the fields, password encyption is moved into another file

--- models/user.js
+++ models/user.js
@@ -1,54 +1,23 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true,
-    trim: true,
     unique: true
-  },
-  name: {
-    type: String,
-    required: true,
-    trim: true
   },
   password: {
     type: String,
     required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
   }
 });
-// Authenticate input against database documents
-UserSchema.statics.authenticate = function(email, password, callback) {
-  User.findOne({ email: email }).exec(function(error, user) {
-    if (error) {
-      return callback(error);
-    } else if (!user) {
-      const err = new Error("User not found");
-      err.status = 401;
-      return callback(err);
-    }
-    bcrypt.compare(password, user.password, function(error, result) {
-      if (result === true) {
-        return callback(null, user);
-      } else {
-        return callback();
-      }
-    });
-  });
-};
-// Hash the password before saving it to database
-UserSchema.pre("save", function(next) {
-  const user = this;
 
-  bcrypt.hash(user.password, 10, function(err, hash) {
-    if (err) {
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  });
-});
-
-const User = mongoose.model("User", UserSchema);
-module.exports = User;
+module.exports = User = mongoose.model("user", UserSchema);

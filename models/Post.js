ef890918839dcb697502c3edd87d202b751d1d8b
Arda Celik <arda.celik@uoit.net>
2019-07-25 16:18:43 -0400

    Post schema is fixed

--- models/Post.js
+++ models/Post.js
@@ -18,17 +18,6 @@ const PostSchema = new Schema({
       user: {
         type: Schema.Types.ObjectId,
         ref: "users"
-      },
-      text: {
-        type: String,
-        required: true
-      },
-      name: {
-        type: String
-      },
-      date: {
-        type: Date,
-        default: Date.now
       }
     }
   ],
